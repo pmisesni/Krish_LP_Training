fcdcca07f5b292c64821afac5c5dc135bf0b7b7c
pmisesni <pmi.sesni@gmail.com>
2021-06-28 20:36:31 +0530

    Simple OAuth project was implemented

---
+++ SpringMicroserviceDemo/SpringMicroserviceDemo-2/src/test/java/com/example/demo/SpringMicroserviceDemo2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringMicroserviceDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
